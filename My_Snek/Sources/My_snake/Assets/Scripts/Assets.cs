b2402bb1f06e5700819872334a1c4a1b3e07d369
DangerousCoffee <a.k.dremov@gmail.com>
2020-05-13 18:43:31 +0300

    Realized movement(buggy) and general scene set up

---
+++ My_Snek/Sources/My_snake/Assets/Scripts/Assets.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Assets : MonoBehaviour
+{
+
+    public static Assets assets;
+    public Sprite bodySprite;
+    public Sprite headSprite;
+    private void Awake()
+    {
+        assets = this;
+    }
+
+}
